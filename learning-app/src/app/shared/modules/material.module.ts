61e2f669c009b73b48af5cb7ab782faa79ee2be1
Ganesh Poojary <ganesh.poojary@robosoftin.com>
2020-08-06 16:28:32 +0530

    header component sample

--- learning-app/src/app/shared/modules/material.module.ts
+++ learning-app/src/app/shared/modules/material.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule} from '@angular/material';
+import { MatButtonModule, MatToolbarModule, MatIconModule, MatMenuModule,
+MatListModule} from '@angular/material';
 
 @NgModule({
   imports: [
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatListModule
   ],
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatListModule
   ]
 })
  export class MaterialModule {}
